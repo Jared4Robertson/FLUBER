33ba68ca508df2b35be3fe9754241adb9d9da834
Jared4Robertson <jared4robertson@gmail.com>
2018-12-08 22:27:33 -0500

    update

--- Final Project/fluber.js
+++ Final Project/fluber.js
@@ -1,5 +1,6 @@
 var root_url ="http://comp426.cs.unc.edu:3001/"
 var airports;
+var airport_names = [];
 var flight_num = 420;
 $(document).ready(()=>{
     $(document).on("click",".login_button",function(){
@@ -20,9 +21,9 @@ $(document).ready(()=>{
     $(document).on("click","#Search_btn",function(){
         show_results();
       })
-    $(document).on("input",".searchbox",function(){
-        autocomplete_airport();
-    })
+    //$(document).on("input",".searchbox",function(){
+     //   autocomplete_airport();
+   // })
       $.ajax(root_url + 'sessions',
       {
       type: 'POST',
@@ -45,6 +46,9 @@ $(document).ready(()=>{
       xhrFields: {withCredentials: true},
       success: (response) => {
           airports=response;
+          for(i=0;i<airports.length;i++){
+              airport_names.push(airports[i].name);
+          }
       },
       error: () => {
           alert('error');
@@ -193,6 +197,9 @@ function add_pass_div(){
     <button id="Search_btn">Search</button>\
     <div id ="mesg_div"</div>\
   </div>')
+  autocomplete(document.getElementById("from"), airport_names);
+  autocomplete(document.getElementById("to"), airport_names);
+
 }
 function show_results(){
     if(pilot_boolean){
@@ -229,14 +236,109 @@ function show_results(){
 
 }
 }
-function autocomplete_airport(){
-    alert(airports[1].text())
-    input = $(event.target).val();
-    for (i = 0; i < airports.length; i++) {
-        /*check if the item starts with the same letters as the text field value:*/
-        if (airports[i][1].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-}}}
+//function autocomplete_airport(){
+  //  alert(airports[0].name)
+    //input = $(event.target.val());
+    //autocomplete(input,airport_names);
+     
+//}
 
 function create_pilot_shit() {
 
 }
+function autocomplete(inp, arr) {
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+    var currentFocus;
+    /*execute a function when someone writes in the text field:*/
+    inp.addEventListener("input", function(e) {
+        var a, b, i, val = this.value;
+        /*close any already open lists of autocompleted values*/
+        closeAllLists();
+        if (!val) { return false;}
+        currentFocus = -1;
+        /*create a DIV element that will contain the items (values):*/
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        /*append the DIV element as a child of the autocomplete container:*/
+        this.parentNode.appendChild(a);
+        /*for each item in the array...*/
+        for (i = 0; i < arr.length; i++) {
+          /*check if the item starts with the same letters as the text field value:*/
+          if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+            /*create a DIV element for each matching element:*/
+            b = document.createElement("DIV");
+            /*make the matching letters bold:*/
+            b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+            b.innerHTML += arr[i].substr(val.length);
+            /*insert a input field that will hold the current array item's value:*/
+            b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+            /*execute a function when someone clicks on the item value (DIV element):*/
+            b.addEventListener("click", function(e) {
+                /*insert the value for the autocomplete text field:*/
+                inp.value = this.getElementsByTagName("input")[0].value;
+                /*close the list of autocompleted values,
+                (or any other open lists of autocompleted values:*/
+                closeAllLists();
+            });
+            a.appendChild(b);
+          }
+        }
+    });
+    /*execute a function presses a key on the keyboard:*/
+    inp.addEventListener("keydown", function(e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+          /*If the arrow DOWN key is pressed,
+          increase the currentFocus variable:*/
+          currentFocus++;
+          /*and and make the current item more visible:*/
+          addActive(x);
+        } else if (e.keyCode == 38) { //up
+          /*If the arrow UP key is pressed,
+          decrease the currentFocus variable:*/
+          currentFocus--;
+          /*and and make the current item more visible:*/
+          addActive(x);
+        } else if (e.keyCode == 13) {
+          /*If the ENTER key is pressed, prevent the form from being submitted,*/
+          e.preventDefault();
+          if (currentFocus > -1) {
+            /*and simulate a click on the "active" item:*/
+            if (x) x[currentFocus].click();
+          }
+        }
+    });
+    function addActive(x) {
+      /*a function to classify an item as "active":*/
+      if (!x) return false;
+      /*start by removing the "active" class on all items:*/
+      removeActive(x);
+      if (currentFocus >= x.length) currentFocus = 0;
+      if (currentFocus < 0) currentFocus = (x.length - 1);
+      /*add class "autocomplete-active":*/
+      x[currentFocus].classList.add("autocomplete-active");
+    }
+    function removeActive(x) {
+      /*a function to remove the "active" class from all autocomplete items:*/
+      for (var i = 0; i < x.length; i++) {
+        x[i].classList.remove("autocomplete-active");
+      }
+    }
+    function closeAllLists(elmnt) {
+      /*close all autocomplete lists in the document,
+      except the one passed as an argument:*/
+      var x = document.getElementsByClassName("autocomplete-items");
+      for (var i = 0; i < x.length; i++) {
+        if (elmnt != x[i] && elmnt != inp) {
+          x[i].parentNode.removeChild(x[i]);
+        }
+      }
+    }
+    /*execute a function when someone clicks in the document:*/
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target);
+    });
+  }
